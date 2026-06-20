c8edb58b32d1ebdbd3da25eb5fdbcc18d4680e9b
bwbioinfo <5053806+bwbioinfo@users.noreply.github.com>
2026-06-20 10:36:40 -0400

    Add Ambxst Hyprland setup

---
+++ dot_config/hypr/hyprland.lua
@@ -0,0 +1,5 @@
+-- Ambxst
+loadfile(os.getenv("HOME") .. "/.local/share/ambxst/hyprland.lua")()
+
+-- OVERRIDES
+-- Down here you can write or source anything that you want to override from Ambxst's settings.
